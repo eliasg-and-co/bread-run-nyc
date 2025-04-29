3c773a9fbcbdb37a1b8e14b8f8685a81596284b2
eliasg-and-co <esg444@gmail.com>
2025-04-29 09:47:04 -0400

    Initial Bread Run NYC

---
+++ src/utils/colors.ts
@@ -0,0 +1,15 @@
+export const cuisineColors: { [key: string]: string } = {
+    "French": "#0055A4",
+    "American": "#3C3B6E",
+    "Mexican": "#006341",
+    "Ethiopian": "#078930",
+    "Chinese": "#DE2910",
+    "Italian": "#008C45",
+    "Indian": "#FF9933",
+    "Japanese": "#BC002D",
+    "Korean": "#003478",
+    "Middle Eastern": "#C8102E",
+    "Caribbean": "#FFD100",
+    "Eastern European": "#0033A0",
+  };
+  
